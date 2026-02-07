dc2b0674cf8014875b6f309ca657018973a0ec5a
Nikenyudha <Nikenyudhaa22@gmail.com>
2026-02-07 13:21:48 +0700

    update code etl

--- etl-script.py
+++ etl-script.py
@@ -1,23 +1,33 @@
 import pandas as pd
 import requests
 from sqlalchemy import create_engine
+import time # Tambahkan ini untuk mengatur jeda waktu
 
-# 1. EXTRACT: Ambil data cuaca Jakarta (Open-Meteo API)
-print("Mengambil data...")
-url = "https://api.open-meteo.com/v1/forecast?latitude=-6.2146&longitude=106.8451&current_weather=true"
-response = requests.get(url).json()
-data = response['current_weather']
-
-# 2. TRANSFORM: Ubah ke DataFrame & rapikan format
-df = pd.DataFrame([data])
-df['city'] = 'Jakarta'
-df['extracted_at'] = pd.Timestamp.now()
-print("Data berhasil dirapikan:")
-print(df)
-
-# 3. LOAD: Kirim ke PostgreSQL
-# Format: postgresql://user:password@localhost:port/database
-# Gunakan user 'postgres' dan port '5435'
+# Setting koneksi
 engine = create_engine('postgresql://postgres:password123@localhost:5435/weather_db')
-df.to_sql('jakarta_weather', engine, if_exists='append', index=False)
-print("Data berhasil disimpan ke Database!")
+
+def run_etl():
+    try:
+        # 1. EXTRACT
+        print(f"[{pd.Timestamp.now()}] Mengambil data...")
+        url = "https://api.open-meteo.com/v1/forecast?latitude=-6.2146&longitude=106.8451&current_weather=true"
+        response = requests.get(url).json()
+        data = response['current_weather']
+
+        # 2. TRANSFORM
+        df = pd.DataFrame([data])
+        df['city'] = 'Jakarta'
+        df['extracted_at'] = pd.Timestamp.now()
+
+        # 3. LOAD
+        df.to_sql('jakarta_weather', engine, if_exists='append', index=False)
+        print("✅ Data berhasil masuk ke Database.")
+    except Exception as e:
+        print(f"❌ Error terjadi: {e}")
+
+# LOOP UTAMA
+if __name__ == "__main__":
+    print("🚀 Pipeline Otomatis Dimulai... (Tekan Ctrl+C untuk berhenti)")
+    while True:
+        run_etl()
+        time.sleep(30) # Tunggu 30 detik sebelum mengambil data lagi
